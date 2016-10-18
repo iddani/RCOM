775c8c6aca9e5bce48ad1686119f493d2da82a25
williamnf <wnfukunaga@gmail.com>
2016-10-18 16:34:08 +0100

    :satisfied::pensive:

--- writer.c
+++ writer.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <signal.h>
 #include <pthread.h>
 
@@ -21,11 +21,13 @@
 #define ADDRESS_RECV 0x01
 #define CONTROL 0x03
 #define BCC 0x02
+#define COUNTER	0x40
 
 
 volatile int STOP=FALSE;
 unsigned int timeouts = 0;
 unsigned int again = FALSE;
+unsigned int ns = 0x00;
 
 
 void alarm_handler(int signo)
@@ -35,13 +37,77 @@ void alarm_handler(int signo)
     again = FALSE;
 }
 
+int readByte(char byte, int status){
+	switch(status){
+		case 0:
+			if(byte == FLAG){
+				return 1;
+			} else return 0;
+			break;
+
+		case 1:
+			if(byte != FLAG){
+				return 2;
+			} else return 1;
+
+			break;
+		case 2:
+			if(byte == FLAG){
+				return 3;
+			} else return 2;
+			break;
+	}
+}
+
+int isUA(char *msg){
+	if(msg[3] != (msg[2]^msg[1])){
+		return -1;
+	} else {
+		if(msg[2] == CONTROL){
+			return TRUE;
+		} else {
+			return 0;
+		}
+
+	}
+}
+
+char* createStartFrame(int fd){
+
+	//fileInfo
+	struct stat s;
+	if (fstat(fd, &s) == -1) {
+  		printf("fstat(%d) returned error=%d.", fd, errno);
+	}
+
+	char packet[20];
+	packet[0] = FLAG;
+	packet[1] = ADDRESS_SEND;
+	ns = (ns^COUNTER);
+	packet[2] = ns;
+	packet[3] = (packet[1]^packet[2]);	//BCC
+	//Data
+	packet[4] = 0x02;		//start
+	packet[5] = 0x00;		//type
+	packet[6] = 0x02;		//number of bytes
+	packet[7] = s.st_size;
+	packet[9] = 0x01;		//name
+	packet[10] = 0x01;
+	packet[11] = 0x01;
+
+printf("%x\n", s.st_size);
+	printf("%x\n", packet[7]);
+	printf("%x\n", packet[8]);
+
+	return packet;
+}
+
 
 int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
-    char buf[255];
-    int i, sum = 0, speed = 0;
+    int sum = 0, speed = 0;
     
     if ( (argc < 2) || 
   	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
@@ -105,44 +171,58 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-	char msg[255];
-	sprintf(msg, "%x%x%x%x%x\n", 
-			FLAG,ADDRESS_SEND, CONTROL, ADDRESS_SEND^CONTROL, FLAG	);
-	res = write(fd, msg, 255);
-	printf("%s\n", msg);
-	alarm(3);
-	char ans[255];
-	res = 0;
-	int byteRead = 0;
-	while((res <= 0) && (timeouts < 3)){
-		
+	char msg[6];
+	msg[0] = FLAG;
+	msg[1] = ADDRESS_SEND;
+	msg[2] = CONTROL;
+	msg[3] = ADDRESS_SEND^CONTROL;
+	msg[4] = FLAG;
+	msg[5] = '\0';
+
+	char buf;
+	char ans[20];
+	int ansLength = 0, status = 0;
+
+	//sprintf(msg, "%x%x%x%x%x\n", FLAG,ADDRESS_SEND, CONTROL, ADDRESS_SEND^CONTROL, FLAG	);
+	for (int i = 0; i < 6; ++i)
+	{
+		printf("%x", msg[i]);
+	}
+	printf("\n");
+	
+	while((STOP == FALSE) && (timeouts < 3)){
 		again = 1;
+		res = write(fd, msg, 6);
+		printf("Wrote %d bytes to fd\n", res);
 		alarm(3);
+		
 		do{
-			res = read(fd, ans, 1);
-			//printf("%s ", ans);
-			
-			if(res >0){	
-				printf("ansX: %x \n",ans);
-				printf("ansD: %d \n",ans);
-			}	
-			if(strcmp(ans, "%x", 0x7E)>= 0)
-				STOP == TRUE;
+			if(res = read(fd, &buf, 1)){
+				status = readByte(buf, status);
+				ans[ansLength] = buf;
+				ansLength++;
+
+				if(status == 3){
+					if(isUA(msg) == TRUE){
+						STOP = TRUE;
+						printf("Connection aknowledged\n");
+					}
+				}
+					
+			}
 		}while(again == TRUE && STOP == FALSE);
 		
-		
 	}
-	printf("res = %d\n", res);
-	
-	printf("leu: %x\n", ans);
+
+
+	int gif = open("pinguim.gif", O_RDONLY);
+	createStartFrame(gif);
+
 
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
-
-
-
 
     close(fd);
     return 0;
