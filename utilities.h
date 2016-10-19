f39504856be19fc98f4a607366a2aac8f7f97dfa
williamnf <wnfukunaga@gmail.com>
2016-10-19 02:49:43 +0100

    Refactoring code

---
+++ utilities.h
@@ -0,0 +1,47 @@
+#define BAUDRATE 		B38400
+#define MODEMDEVICE 	"/dev/ttyS1"
+#define _POSIX_SOURCE 	1 /* POSIX compliant source */
+#define FALSE 			0
+#define TRUE 			1
+
+#define COUNTER_MODULE	0x40
+
+#define FLAG 			0x7E
+#define ADDRESS_SEND 	0x03
+#define ADDRESS_RECV 	0x01
+//#define BCC 			0x02
+
+#define	CTRL_SET		0x03
+#define	CTRL_DISC		0x0B
+#define	CTRL_UA			0x07
+
+#define I_CTRL_DATA		0x01
+#define I_CTRL_START	0x02
+#define I_CTRL_END		0x03
+
+//#define	CTRL_RR(ns)	(ns == 0 ? 0x0)			
+
+
+
+typedef enum{
+	TRANSMITTER, RECEIVER
+}ConnectionMode;
+
+
+typedef enum {
+	SET, DISC, UA, RR, REJ 
+}ControlType;
+
+struct applicationLayer {
+	int fd;			/*Descritor correspondente à porta série*/
+	ConnectionMode transmission;	/*TRANSMITTER | RECEIVER*/
+};
+
+struct linkLayer {
+	char port[20]; 	/*Dispositivo /dev/ttySx, x = 0, 1*/
+	int baudRate;	/*Velocidade de transmissão*/
+	unsigned int sequenceNumber;  	 /*Número de sequência da trama: 0, 1*/
+	unsigned int timeout;			 /*Valor do temporizador: 1 s*/
+	unsigned int numTransmissions;	 /*Número de tentativas em caso de falha*/
+	char frame[20];			/*Trama*/
+};
--- writer.c
+++ writer.c
@@ -10,25 +10,15 @@
 #include <string.h>
 #include <signal.h>
 #include <pthread.h>
-
-#define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-#define FLAG 0x7E
-#define ADDRESS_SEND 0x03
-#define ADDRESS_RECV 0x01
-#define CONTROL 0x03
-#define BCC 0x02
-#define COUNTER	0x40
-
+#include <unistd.h>
+#include "utilities.h"
 
 volatile int STOP=FALSE;
 unsigned int timeouts = 0;
 unsigned int again = FALSE;
 unsigned int ns = 0x00;
-
+struct termios oldtio,newtio;
+struct applicationLayer appLayer;
 
 void alarm_handler(int signo)
 {
@@ -59,112 +49,140 @@ int readByte(char byte, int status){
 	}
 }
 
-int isUA(char *msg){
-	if(msg[3] != (msg[2]^msg[1])){
+
+ControlType analyse(char *msg){
+
+	char address = msg[1], control = msg[2], bcc = msg[3];
+	if(bcc != (address^control)){
 		return -1;
 	} else {
-		if(msg[2] == CONTROL){
-			return TRUE;
+		if(control == CTRL_SET){
+			return SET;
 		} else {
 			return 0;
 		}
-
 	}
 }
 
-int disconnect(fd){
-	
+int sendMessage(ControlType type, char *msg){
+	char buf;
+	char ans[20];
+	int ansLength = 0, status = 0;
+	int res = 0;
+
+	while((STOP == FALSE) && (timeouts < 3)){
+		again = 1;
+		res = write(appLayer.fd, msg, 6);
+		printf("Wrote %d bytes to fd\n", res);
+
+		alarm(3);
+		
+		while(again == TRUE && STOP == FALSE){
+			if(res = read(appLayer.fd, &buf, 1)){
+				status = readByte(buf, status);
+				ans[ansLength] = buf;
+				ansLength++;
+
+				if(status == 3){
+					if(analyse(msg) == TRUE){
+						STOP = TRUE;
+						printf("Disconnect aknowledged\n");
+						//alarm(0)		//desativa flag
+					}
+				}
+					
+			}
+		};
+	}
 }
 
-char* createStartFrame(int fd, char *name){
-
-	//fileInfo
+char *createControlPacket(int type){
+		//fileInfo
 	struct stat s;
-	if (fstat(fd, &s) == -1) {
-  		printf("fstat(%d) returned error=%d.", fd, errno);
+	if (fstat(appLayer.fd, &s) == -1) {
+  		printf("fstat(%d) returned error=%d.", appLayer.fd, errno);
 	}
 
-	char packet[20];
+	char packet[10];
 	packet[0] = FLAG;
-	packet[1] = ADDRESS_SEND;
-	ns = (ns^COUNTER);
+
+	if(appLayer.transmission == TRANSMITTER){
+		packet[1] = ADDRESS_SEND;
+	} else{		/*Receiver*/
+		packet[1] = ADDRESS_RECV;
+	}
+	ns = (ns^COUNTER_MODULE);
 	packet[2] = ns;
 	packet[3] = (packet[1]^packet[2]);	//BCC
 	//Data
-	packet[4] = 0x02;		//start
+	packet[4] = type;		//start
 	packet[5] = 0x00;		//type
-	packet[6] = 0x02;		//number of bytes
-	packet[7] = s.st_size;
+	//packet[6] = 0x02;		//number of bytes
+	//packet[7] = s.st_size;
 	packet[9] = 0x01;		//name
-	packet[10] = 0x01;		//number of bytes
-	packet[11] = name;		//value
-
-printf("%x\n", s.st_size);
-	printf("%x\n", packet[7]);
-	printf("%x\n", packet[8]);
+	//packet[10] = 0x01;		//number of bytes
+	//packet[11] = name;		//value
 
 	return packet;
 }
 
-char* createEndFrame(int fd, char *name){
+char *createSUFrame(ControlType type){
 
-	//fileInfo
-	struct stat s;
-	if (fstat(fd, &s) == -1) {
-  		printf("fstat(%d) returned error=%d.", fd, errno);
+	char frame[6];
+	frame[0] = FLAG;
+	frame[4] = FLAG;
+	frame[5] = '\0';
+
+	switch(appLayer.transmission){
+		case TRANSMITTER:
+			frame[1] = ADDRESS_SEND;
+			break;
+		case RECEIVER:
+			frame[1] = ADDRESS_RECV; 
+			break;
 	}
 
-	char packet[20];
-	packet[0] = FLAG;
-	packet[1] = ADDRESS_SEND;
-	ns = (ns^COUNTER);
-	packet[2] = ns;
-	packet[3] = (packet[1]^packet[2]);	//BCC
-	//Data
-	packet[4] = 0x03;		//end
-	packet[5] = 0x00;		//type
-	packet[6] = 0x02;		//number of bytes
-	packet[7] = s.st_size;
-	packet[9] = 0x01;		//name
-	packet[10] = 0x01;		//number of bytes
-	packet[11] = name;		//value
+	switch(type){
+		case SET:
+			frame[2] = CTRL_SET;
+			break;
+		case DISC:
+			frame[2] = CTRL_DISC;
+			break;
+		case UA:
+			frame[2] = CTRL_UA;
+			break;
+		case RR:
 
+			break;
+		case REJ:
 
-
-printf("%x\n", s.st_size);
-	printf("%x\n", packet[7]);
-	printf("%x\n", packet[8]);
-
-	return packet;
+			break;
+	}
+	return frame;
 }
 
+int llopen(int gate, ConnectionMode connection){
 
-int main(int argc, char** argv)
-{
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    int sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-    }
+	char port[10];
+	if(gate == 0){
+		sprintf(port, "/dev/ttyS0");
+	} else if(gate == 1){
+		sprintf(port, "/dev/ttyS1");
+	}
+	printf("%s\n", port);
 
+	int fd = open(port, O_RDWR | O_NOCTTY );
 
-  /*
+    if (fd <0) {perror(port); exit(-1); }
+
+    /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
-  */
-
-
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
-
+  	*/
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
+      	perror("tcgetattr");
+      	exit(-1);
     }
 
     bzero(&newtio, sizeof(newtio));
@@ -179,23 +197,10 @@ int main(int argc, char** argv)
     newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
 
 
-
-  /* 
+  	/* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
     leitura do(s) próximo(s) caracter(es)
-  */
-
-    struct sigaction action;
-    action.sa_handler = alarm_handler;
-    sigemptyset(&action.sa_mask);
-    action.sa_flags = 0;
-
-    if (sigaction(SIGALRM,&action, NULL) < 0)
-    {
-        fprintf(stderr,"Unable to install SIGALRM handler\n");
-        exit(1);
-    }
-
+  	*/
 
 
     tcflush(fd, TCIOFLUSH);
@@ -206,26 +211,15 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
+	
 
-	char msg[6];
-	msg[0] = FLAG;
-	msg[1] = ADDRESS_SEND;
-	msg[2] = CONTROL;
-	msg[3] = ADDRESS_SEND^CONTROL;
-	msg[4] = FLAG;
-	msg[5] = '\0';
 
+	char msg = createSUFrame(SET);
 	char buf;
 	char ans[20];
 	int ansLength = 0, status = 0;
+	int res = 0;
 
-	//sprintf(msg, "%x%x%x%x%x\n", FLAG,ADDRESS_SEND, CONTROL, ADDRESS_SEND^CONTROL, FLAG	);
-	for (int i = 0; i < 6; ++i)
-	{
-		printf("%x", msg[i]);
-	}
-	printf("\n");
-	
 	while((STOP == FALSE) && (timeouts < 3)){
 		again = 1;
 		res = write(fd, msg, 6);
@@ -238,8 +232,8 @@ int main(int argc, char** argv)
 				ans[ansLength] = buf;
 				ansLength++;
 
-				if(status == 3){
-					if(isUA(msg) == TRUE){
+				if(status == 3){ ;
+					if(analyse(msg) == TRUE){
 						STOP = TRUE;
 						printf("Connection aknowledged\n");
 					}
@@ -247,23 +241,84 @@ int main(int argc, char** argv)
 					
 			}
 		}while(again == TRUE && STOP == FALSE);
-		
+	}
+	return fd;	
+}
+
+
+int llclose(int fd){
+	char msg = createSUFrame(DISC);
+
+	sendMessage(DISC, msg);
+
+
+}
+
+
+int main(int argc, char** argv)
+{
+    int fd, res;
+    int sum = 0, speed = 0;
+
+    
+    if ( (argc < 3) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort ConnectionMode\n\tex: nserial /dev/ttyS1 TRANSMITTER||RECEIVER\n");
+      exit(1);
+    }
+
+    ConnectionMode connection;
+    int gate;
+    if(strcmp("/dev/ttyS0", argv[1])==0){
+    	gate = 0;
+    } else if(strcmp("/dev/ttyS1", argv[1])==0){
+    	gate = 1;
+    } else{
+   		printf("SerialPort not recognized\n\tuse: /dev/ttyS0 || /dev/ttyS1\n");
+      	exit(1);
+    }
+
+    if(strcmp("TRANSMITTER", argv[2])==0){
+    	appLayer.transmission = TRANSMITTER;
+	} else if(strcmp("RECEIVER", argv[2])==0){
+		appLayer.transmission = RECEIVER;
+	} else{
+		printf("ConnectionMode not recognized\n\tuse: TRANSMITTER||RECEIVER\n");
+      	exit(1);
 	}
 
+    struct sigaction action;
+    action.sa_handler = alarm_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
 
+    if (sigaction(SIGALRM,&action, NULL) < 0)
+    {
+        fprintf(stderr,"Unable to install SIGALRM handler\n");
+        exit(1);
+    }
+
+    appLayer.fd = llopen(gate, connection);
+
+
+	
+/*
 	int gif = open("pinguim.gif", O_RDONLY);
 	createStartFrame(gif,"pinguim.gif" );
 
 	char pic;
 	char data[255];
 	int it = 0;
+*/
 
+/*
 	while(it < 255){
-		llread( gif, pic, 1);
+		read( gif, pic, 1);
 		if(pic == FLAG){
 			data[it] = 0x7D;
 			data[++it] = 0x5E;
-		} else if (pic == 0x7D{
+		} else if (pic == 0x7D){
 			data[it] = 0x7D;
 			data[++it] = 0x5D;
 		} else{
@@ -276,7 +331,7 @@ int main(int argc, char** argv)
 			it =0;
 		}
 	}
-
+*/
 
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
